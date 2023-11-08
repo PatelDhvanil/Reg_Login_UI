3f6da12a9969117bf44ef38469f38a6870a04ed8
Dhvanil Patel <139988591+Dhvanil2503@users.noreply.github.com>
2023-11-08 14:50:12 +0530

    Add the project files

---
+++ Reg_Login/app/src/main/java/com/example/reg_login/Login.java
@@ -0,0 +1,25 @@
+package com.example.reg_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView btn=findViewById(R.id.textviewsignup);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,Registration.class));
+            }
+        });
+    }
+}
